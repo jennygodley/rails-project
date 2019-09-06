eeed6d781a0b9bf07d7b2c617f4f02793f484271
jennygodley <jennifergodley@gmail.com>
2019-09-06 10:41:51 -0400

    Add words table
    Added migrations to set up words table and connect it to the users
    table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 2019_09_06_143121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,14 @@ ActiveRecord::Schema.define(version: 2) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  create_table "words", force: :cascade do |t|
+    t.string "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_words_on_user_id"
+  end
+
   add_foreign_key "examples", "users"
+  add_foreign_key "words", "users"
 end
